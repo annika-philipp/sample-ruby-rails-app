f77a009387ae7b9876d871589226510d0918d064
Annika <liebste.anni@gmail.com>
2018-07-02 19:55:03 +1200

    changes picture_uploader

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -5,11 +5,14 @@ class PictureUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [400, 400]
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  storage :file
+
+  # I'd really want the below, look into AWS S3 when time
+  # if Rails.env.production?
+  #   storage :fog
+  # else
+  #   storage :file
+  # end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
